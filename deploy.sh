2d350fb8a8f0d207141c1fa8b903ed4a967cdfd8
zhongshirong <zhongshirong@zdnst.gz>
2021-04-13 19:22:37 +0800

    *

--- deploy.sh
+++ deploy.sh
@@ -19,6 +19,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:coco-h5/coco-global-banner.git master:gh-pages
+git push -f git@github.com:ooh5/coco-global-banner.git master:gh-pages
 
 cd -
