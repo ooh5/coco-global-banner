6e1549c4b8be77ed06a182bd0779591b9bcd7096
zhongshirong <zhongshirong@zdnst.gz>
2021-04-13 18:57:53 +0800

    build test

--- coco.config.js
+++ coco.config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  webDomian: 'https://coco-h5.github.io/coco-global-banner',
-  gitUrl: 'git@github.com:coco-h5/coco-global-banner.git',
+  webDomian: 'https://ooh5.github.io/coco-global-banner',
+  gitUrl: 'git@github.com:ooh5/coco-global-banner.git',
   name: 'coco-global-banner',
   description: "远程banner组件"
 }
